f0126f6dbdaa5f1f798f39a59967c4cb68559b4d
satyPunk <satyamhere8@gmail.com>
2025-03-29 23:24:53 +0530

    Created all routes {tentative}

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ReactNode } from "react";
+const RootLayout = ({ children }: { children: ReactNode }) => {
+    return(
+        <div>{children}</div>
+    );
+}
+export default RootLayout;
