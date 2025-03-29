7d9b98cdc1d4673c03e651d44cda64cdd01fb31f
satyPunk <satyamhere8@gmail.com>
2025-03-29 17:00:26 +0530

    Created the auth routes

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,10 @@
+import React, { ReactNode } from 'react';
+
+const AuthLayout = ({children}:{children:ReactNode}) => {
+  return (
+    <div>
+      {children}
+    </div>
+  );
+}
+export default AuthLayout;
