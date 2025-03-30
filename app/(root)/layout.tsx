26ddefe53c42400d9229df2acf69f1dd9db86a58
satyPunk <satyamhere8@gmail.com>
2025-03-31 00:24:09 +0530

    Login page design done, home page banner set

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import { ReactNode } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Page from "./page";
 const RootLayout = ({ children }: { children: ReactNode }) => {
     return(
-        <div>{children}</div>
+        <div className='root-layout'>
+            <nav>
+                <Link href ='/' className='flex items-center gap-2'>
+                <Image src="/logo.svg" alt="Logo" width={32} height={32} className="rounded-full" />
+                <h2 className="text-primary-100">InterVue AI</h2>
+                </Link>
+            </nav>
+            {/* <Page/> */}
+            {children}
+        </div>
     );
 }
 export default RootLayout;
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,27 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 import React from 'react';
-
+import Image from 'next/image';
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <>
+    <section className='card-cta'>
+      <div className='flex flex-col gap-6 max-w-lg'>
+        <h2>Practice mock interviews or take AI-powered speech tests with real-time feedback and insights. </h2>
+        <p>Practice real interview questions and get feedback</p>
+        <Button asChild className='btn-primary max-sm:w-full'>
+          <Link href="/interview">Start an Interview</Link>
+          </Button>
+      </div>
+      <Image
+      src='/robot.png'
+      alt='robot'
+      width={400}
+      height={400}
+      className='max-sm:hidden'
+      />
+    </section>
+    </>
   );
 }
 export default Page;
