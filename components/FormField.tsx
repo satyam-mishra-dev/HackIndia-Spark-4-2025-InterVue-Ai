e36a7ebc4bbd0011c89ec1849465adddc5522b61
satyPunk <satyamhere8@gmail.com>
2025-03-30 12:41:20 +0530

    Small blunder

---
+++ components/FormField.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+const Form = () => {
+    return (
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+    );
+}
+export default Form;
