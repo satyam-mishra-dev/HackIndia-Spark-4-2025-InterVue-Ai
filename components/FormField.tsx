468c8135076c02d5b4c8ed70ec2c24ae3665f48b
satyPunk <satyamhere8@gmail.com>
2025-03-31 20:42:58 +0530

    tried to solve error

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,32 +1,33 @@
-import React from "react";
-import { Controller, FieldValues, useForm,Control,Path } from "react-hook-form";
+import React, { FC } from "react";
+import { Controller, Control, Path } from "react-hook-form";
 import { FormControl, FormDescription, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { FieldValues } from "react-hook-form";
 interface FormFieldProps<T extends FieldValues> {
     control: Control<T>;
-    name:Path<T>;
-    label :string;
-    placeholder?:string;
-    type?:'text'|'email'|'password'|'file';
-    description?:string;
+    name: Path<T>;
+    label: string;
+    placeholder?: string;
+    type?: 'text' | 'email' | 'password' | 'file';
+    description?: string;
 }
-const FormField = ({control,name,label,placeholder,description,type="text"}:FormFieldProps<T>) => {
+
+const FormField: FC<FormFieldProps<T>> = ({ control, name, label, placeholder, description, type = "text" }) => {
     return (
-        <Controller name={name} control ={control} render={({ field }) => (
-          <FormItem>
-              <FormLabel className="label">{label}</FormLabel>
-              <FormControl>
-                <Input className="input text-slate-400" placeholder={placeholder} {...field} />
-              </FormControl>
-              <FormDescription>
-                {description}
-              </FormDescription>
-              <FormMessage />
+        <Controller name={name} control={control} render={({ field }) => (
+            <FormItem>
+                <FormLabel className="label">{label}</FormLabel>
+                <FormControl>
+                    <Input className="input text-slate-400" placeholder={placeholder} {...field} />
+                </FormControl>
+                <FormDescription>
+                    {description}
+                </FormDescription>
+                <FormMessage />
             </FormItem>
         )}
-            
-         
         />
     );
 }
+
 export default FormField;
